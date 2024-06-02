a2ba3dcf13ca5104aacd1e902d5d09aa84ca6b11
stepheeeen <stephenon56@gmail.com>
2024-06-02 17:32:38 +0100

    tailwind configuration

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import tw from 'twrnc'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={tw`text-[24px]`}>hello World it is!!</Text>
       <StatusBar style="auto" />
     </View>
   );
